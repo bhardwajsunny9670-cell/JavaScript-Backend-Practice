d44933174cc2434a400a1828e754d6a48de1a327
Sunny Sharma <bhardwajsunny9670@gmail.com>
2026-07-05 11:00:15 +0530

    feat: add fs module sync file manager

---
+++ nodejs-core/task4/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+fs.writeFileSync("notes.txt","Hello Node");
+
+const data = fs.readFileSync('notes.txt', 'utf8');
+console.log(data);
+ 
+fs.appendFileSync("notes.txt","- updated","utf8");
+ console.log("file updated successfully");
+
+ fs.renameSync("notes.txt","notes-backup.txt");
+ console.log("file rename successfully");
+
+fs.unlinkSync("notes-backup.txt");
+console.log("deleted successfully");
+
+try{
+     fs.readFileSync("ghost.txt","utf8")
+    console.log("file read successfully")
+}catch(err){
+    if(err.code === 'ENOENT'){
+        console.log("file not found")
+    }else{
+        console.log("Something unexpected happens");
+    }
+}
